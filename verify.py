fada46812c844d5c2592b0c89758376c23c9af5b
Giampiero Salvi <giampisalvi@gmail.com>
2023-04-05 14:14:24 +0200

    added test on wav files

--- verify.py
+++ verify.py
@@ -8,6 +8,7 @@ csv_file = sys.argv[1]
 audio_path = sys.argv[2]
 
 df = pd.read_csv(csv_file)
+print('Checking that every audio file from', csv_file, 'exists...')
 for index, row in df.iterrows():
     # check if audio file exists
     audio_file_name = audio_path + '/' + row['File name']
@@ -16,3 +17,10 @@ for index, row in df.iterrows():
         alternatives = glob.glob(audio_file_name[:-4] + '*.wav')
         if len(alternatives):
             print('Found alternatives: ', alternatives)
+print('Listing files in', audio_path, 'without an assessment:')
+filenames = set(df['File name'])
+for filename in glob.glob(audio_path + '/*.wav'):
+    fn = filename.split('/')[1]
+    if fn not in filenames:
+        print(fn)
+
